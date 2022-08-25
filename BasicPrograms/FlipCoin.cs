a8b30f5bfeeea51b648c7e4b4670f3bb86056e4a
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-25 19:20:33 +0530

    [Prajakta] Add: FlipCoin

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class FlipCoin
+    {
+        public void ReadInput()
+        {
+            Console.WriteLine("Please Enter The Number Of Times To Flip Coin : ");
+            string input = Console.ReadLine();
+            int numbofTimes = Convert.ToInt32(input);
+            Flip(numbofTimes);  
+        }
+        private static void Flip(int numofTimes)
+        {
+            int tail = 0; int head = 0; 
+            Random random = new Random();
+            for (int i = 1; i <= numofTimes; i++)
+            {
+                if (random.NextDouble() < 0.5)
+                    tail++;      
+                else
+                    head++;    
+            }
+            double tailpercentage = (double)tail / numofTimes * 100;  
+            double headpercentage = (double)head / numofTimes * 100;
+            Console.WriteLine("Tail count" + "=" + tail + "\tpercentage " + tailpercentage); 
+            Console.WriteLine("head count" + "=" + head + "\tpercentage " + headpercentage);
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Basic Core Programs");
+            Console.WriteLine("Please Select Your Choice \n 1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.ReadInput();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
