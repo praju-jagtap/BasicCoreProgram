126bf755d8ed39ff84f40a7bf799b182effea5d1
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-25 20:21:12 +0530

    [Prajakta] Add: Largest Among Three Numbers

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Select Your Choice \n 1.FlipCoin \n 2.EvenOdd");
+            Console.WriteLine("Please Select Your Choice \n 1.FlipCoin \n 2.EvenOdd \n 3.Largest Among Three Numbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -15,6 +15,10 @@
                 case 2:
                     EvenOdd evenOdd = new EvenOdd();
                     evenOdd.Evenodd();
+                    break;
+                case 3:
+                    LargestNo largeNo = new LargestNo();
+                    largeNo.Largest();
                     break;
                 default:
                     break;
